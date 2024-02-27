7676e4fc3a3f17e997e2918f5dc0055da6f3a6b0
@hexlet/cli <support@hexlet.io>
2024-02-27 23:29:22 +0300

    submit java-oop-ru/inheritance

---
+++ java-oop-ru/inheritance/src/main/java/exercise/PairedTag.java
@@ -0,0 +1,27 @@
+package exercise;
+
+import java.util.Map;
+import java.util.List;
+
+// BEGIN
+public class PairedTag extends Tag{
+    private String body;
+    private List<Tag> chilList;
+    public PairedTag(String name, Map<String, String> attributes, String body, List<Tag> chilList) {
+        super(name, attributes);
+        this.body = body;
+        this.chilList = chilList;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder(super.toString());
+        builder.append(body);
+        for (Tag tag : chilList){
+            builder.append(tag.toString());
+        }
+        builder.append("</").append(name).append(">");
+        return builder.toString();
+    }
+}
+// END
---
+++ java-oop-ru/inheritance/src/main/java/exercise/Tag.java
@@ -0,0 +1,26 @@
+package exercise;
+
+import java.util.stream.Collectors;
+import java.util.Map;
+
+// BEGIN
+public abstract class Tag {
+    protected String name;
+    protected Map<String, String> attributes;
+
+    public Tag(String name, Map<String, String> attributes) {
+        this.name = name;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder("<" + name);
+        for (String key : attributes.keySet()) {
+            builder.append(String.format(" %s=\"%s\"",key, attributes.get(key)));
+        }
+        builder.append(">");
+        return builder.toString();
+    }
+}
+// END
