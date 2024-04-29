bac1c6e1efa1a8986a6661ee98f7a173d3ffc60e
@hexlet/cli <support@hexlet.io>
2024-04-29 19:00:21 +0300

    submit java-web-ru/cookies

--- java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
+++ java-web-ru/cookies/src/main/java/exercise/controller/UsersController.java
@@ -6,6 +6,7 @@ import exercise.repository.UserRepository;
 import exercise.util.NamedRoutes;
 import exercise.util.Security;
 import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
 
 import static io.javalin.rendering.template.TemplateUtil.model;
 
@@ -18,12 +19,14 @@ public class UsersController {
 
     public static void show(Context ctx) {
         var id = ctx.pathParamAsClass("id", Long.class).get();
-        User user = UserRepository.find(id).get();
+        var token = ctx.cookie("token") == null ? null : ctx.cookie("token");
+        User user = UserRepository.find(id).orElseThrow(() -> new NotFoundResponse("User not found"));
+        if (user == null || !user.getToken().equals(token)) {
+            ctx.redirect(NamedRoutes.usersPath());
+            return;
+        }
         UserPage page = new UserPage(user);
-        String token = user.getToken();
-        if (token.equals(ctx.cookie("userToken"))) {
-            ctx.render("users/show.jte", model("page", page));
-        } else ctx.redirect(NamedRoutes.usersPath());
+        ctx.render("users/show.jte", model("page", page));
     }
 
     // BEGIN
@@ -33,11 +36,10 @@ public class UsersController {
         var email = ctx.formParam("email");
         var password = ctx.formParam("password");
         var token = Security.generateToken();
-        var user = new User(firstName, lastName, email, password, token);
+        var user = new User(firstName, lastName, email, Security.encrypt(password), token);
         UserRepository.save(user);
-        long id = UserRepository.getEntities().size();
-        ctx.cookie("userToken", token);
-        ctx.redirect(NamedRoutes.userPath(id));
+        ctx.cookie("token", token);
+        ctx.redirect(NamedRoutes.userPath(user.getId()));
     }
     // END
 }
