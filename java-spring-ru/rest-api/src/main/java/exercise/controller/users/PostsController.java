a12ef4e3403b73f035888861eec4e92ec052b0ff
@hexlet/cli <support@hexlet.io>
2024-06-09 17:11:01 +0300

    submit java-spring-ru/rest-api

---
+++ java-spring-ru/rest-api/src/main/java/exercise/controller/users/PostsController.java
@@ -0,0 +1,42 @@
+package exercise.controller.users;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import exercise.model.Post;
+import exercise.Data;
+
+// BEGIN
+@RestController
+@RequestMapping("/api")
+public class PostsController {
+    private List<Post> list = new ArrayList<>();
+
+    @GetMapping("/users/{id}/posts")
+    ResponseEntity<List<Post>> index(@PathVariable int id) {
+        List<Post> result = list.stream().filter(post -> post.getUserId() == id).toList();
+        HttpStatus status = HttpStatus.NO_CONTENT;
+        if (!result.isEmpty()) {
+            status = HttpStatus.OK;
+        }
+        return ResponseEntity.status(status).body(result);
+    }
+
+    @PostMapping("/users/{id}/posts")
+    @ResponseStatus(HttpStatus.CREATED)
+    Post create(@PathVariable int id, @RequestBody Post data) throws URISyntaxException {
+        Post post = new Post();
+        post.setUserId(id);
+        post.setTitle(data.getTitle());
+        post.setSlug(data.getSlug());
+        post.setBody(data.getBody());
+        list.add(post);
+        return post;
+    }
+}
+// END
